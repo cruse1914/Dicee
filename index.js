d1cf648e14698150d367ca78d1d16b5415c607af
Dipayan Mukherjee <56663132+cruse1914@users.noreply.github.com>
2021-03-15 19:08:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ function rollDice() {
 	document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 	if (randomNumber1 > randomNumber2) {
-		document.querySelector("h1").innerHTML = "Player 1 wins! 🚩";
+		document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
 	} else if (randomNumber2 > randomNumber1) {
-		document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+		document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 	} else {
-		document.querySelector("h1").innerHTML = "Draw! 🤝";
+		document.querySelector("h1").innerHTML = "Draw!";
     }
 }
 
